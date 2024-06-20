c67dd8cbde8d3d53b6c2eb1896a8fc6a374ce2ca
Nimnadee <snr.nimnadi@gmail.com>
2024-06-20 15:32:30 +0530

    created basic student account creating form

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -1,6 +1,20 @@
+"use client";
 import React from "react";
-import { Input } from "@nextui-org/react";
+import StudentForm from "@/app/student/student-form";
+import { Student } from "@/service/student";
+import {Project} from "@/service/project.service";
 
 export default function StudentPage() {
 
+    const handleSave = (student: Student) => {
+        console.log("Trying to save: ");
+
+    };
+
+
+  return (
+    <>
+      <StudentForm onSave={handleSave} />
+    </>
+  );
 }
