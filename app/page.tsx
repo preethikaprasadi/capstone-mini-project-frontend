4b1a0259da6f1f965db032cb70848e33d6dadf31
Nimnadee <snr.nimnadi@gmail.com>
2024-07-14 10:29:47 +0530

    merge-113c30c

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,18 @@ import { Navbar } from "@/app/components/navbar";
 import { Student } from "@/service/student.service";
 import NewStudentSignupPopup from "@/app/student/new-student-signup-popup";
 import GuideLoginPopup from "@/app/guide/login-popup";
- 
- 
+import TypeWrite from "./Typewriteeffect";
+
 
 export default function Home() {
 
   const handleSave = (student: Student) => {
     console.log("Trying to save: ", student);
   };
-   
+
   return (
       <>
-        
+
         <Navbar />
         <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
           <div className="inline-block max-w-lg text-center justify-center">
@@ -27,12 +27,11 @@ export default function Home() {
             <h1 className={title()}>with </h1>
             <h1 className={title({ color: "violet" })}>Guidly</h1>
             <br />
-            <h2 className={subtitle({ class: "mt-6" })}>
-              Connecting University Students and Guiders Together
-            </h2>
+
+            <TypeWrite/>
           </div>
 
-          
+
           <div className="flex gap-3">
             <NewStudentSignupPopup onSave={handleSave}/>
             <GuideLoginPopup onSave={undefined}/>
